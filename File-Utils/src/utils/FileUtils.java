304c8f9ba5de00e7d1d419d6009f19de857c67e8
jaykybert <51761831+jaykybert@users.noreply.github.com>
2019-07-11 17:07:14 +0100

    Added method to write to a file.

--- File-Utils/src/utils/FileUtils.java
+++ File-Utils/src/utils/FileUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.*;
+/* Change import statement (specify imports) */
 
 
 public class FileUtils {
@@ -9,10 +10,11 @@ public class FileUtils {
 	 * Take x files and reads each one, returning an array
 	 * containing the content of each file as a string.
 	 * @param filenames: x amount of files.
-	 * @return fileContents: Array of the files' content.
+	 * @return allContents: Array of the files' content.
 	 */
 	String[] readFiles(String ... filenames) {
-		String[] fileContents = new String[filenames.length];
+		
+		String[] allContents = new String[filenames.length];
 		StringBuilder fileContent = new StringBuilder();
 		String contentLine;
 			
@@ -23,19 +25,53 @@ public class FileUtils {
 				while((contentLine = readFile.readLine()) != null) {
 					fileContent.append(contentLine + "\n");
 				}
-				fileContents[i] = fileContent.toString(); // Store file contents.
+				allContents[i] = fileContent.toString(); // Store file contents.
 				fileContent.setLength(0); // Reset StringBuilder.
 			}
 		
-			catch(IOException exc) {
-				System.out.println(exc);
+			catch(IOException ex) {
+				System.out.println(ex);
 			}
 			
 		}
-		return fileContents;
+		return allContents;
 	}
+	
+	/**
+	 * Read input from the keyboard and write it to a file.
+	 * @param filename: name of the file (and path).
+	 * @param append: Determines whether to overwrite or append to the file.
+	 */
+	void writeFile(String filename, boolean append) {
+		
+		BufferedReader readKeyboard = new BufferedReader(new InputStreamReader(System.in));
+		boolean keepReading = true;
+		
+		String keyboardInput;
 
-
+		try(FileWriter writeFile = new FileWriter(filename, append)) {
+			System.out.println("File Created\nEnter text (enter exit to quit).");
+			
+			while(keepReading) {
+				System.out.print("> ");
+				keyboardInput = readKeyboard.readLine();
+				
+				if (keyboardInput.contentEquals("exit")) {
+					keepReading = false;
+				}
+				else {
+					writeFile.write(keyboardInput+"\n");
+				}	
+			}
+			System.out.println("Text successfully written.");
+		}
+		catch(IOException ex) {
+			System.out.println(ex);
+		}
+		
+		
+		
+	}
 
 
 
--- File-Utils/src/utils/FileUtilsTest.java
+++ File-Utils/src/utils/FileUtilsTest.java
@@ -5,13 +5,20 @@ public class FileUtilsTest {
 	public static void main(String[] args) {
 		
 		FileUtils f = new FileUtils();
-		
 		String[] filenames = {"Files/f1.txt", "Files/f2.txt", "Files/f3.txt"};
+		
+		/* ReadFiles test.
 		String[] results = f.readFiles(filenames);
 		
 		for(String result: results) {
 			System.out.println("--------------------");
 			System.out.println(result);
-		}	
+		}
+		*/
+		
+		f.writeFile("wontwork.txt", true);
+		
+		
+		
 	}
 }
