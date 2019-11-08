8a04647a729bb22ded552cf377976ba3cadd0160
jaykybert <kybertjay@gmail.com>
2019-11-08 18:21:42 +0000

    Added .java file for desktop GUI.

---
+++ File-Utils/src/utils/FileUtilsGUI.java
@@ -0,0 +1,20 @@
+package utils;
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+
+public class FileUtilsGUI extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage stage) {
+		stage.setTitle("File Utlities");
+		
+		
+		stage.show();
+	}
+}
