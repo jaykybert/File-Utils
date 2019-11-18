56cf285eb0f072c3b96b691e7d9cc72109bfd633
jaykybert <51761831+jaykybert@users.noreply.github.com>
2019-11-18 19:36:53 +0000

    Added button file choosing, connected compareFiles with GUI.

--- File-Utils/src/utils/FileUtilsGUI.java
+++ File-Utils/src/utils/FileUtilsGUI.java
@@ -1,13 +1,22 @@
 package utils;
 
+import java.io.File;
+
 import javafx.application.*;
 import javafx.geometry.Orientation;
 import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.*;
 
+import utils.FileUtils;
 
 public class FileUtilsGUI extends Application {
+	
+	String filepath1 = "";
+	
+	String filepath2 = "";
+	
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -15,14 +24,51 @@ public class FileUtilsGUI extends Application {
 	
 	public void start(Stage stage) {
 		stage.setTitle("File Utlities");
+		
+		FlowPane rootNode = new FlowPane(Orientation.VERTICAL);
+		Label label = new Label("");
 
+		Button chooseButton1 = new Button("File 1");
+		Button chooseButton2 = new Button("File 2");
+		Button compareFiles = new Button("Compare");
+		
 		FileChooser file1 = new FileChooser();
-		file1.setTitle("File 1");
-		// Put this inside a button event handler.
-		file1.showOpenDialog(stage);
-		FlowPane root = new FlowPane(Orientation.VERTICAL);
-		Scene scene = new Scene(root, 300, 400);
+		FileChooser file2 = new FileChooser();
+		file1.setTitle("Select File 1");
+		file2.setTitle("Select File 2");
+		
+		
+		rootNode.getChildren().addAll(label, chooseButton1, chooseButton2, compareFiles);
+		Scene scene = new Scene(rootNode, 300, 400);
 		stage.setScene(scene);
 		stage.show();
-	}
+		
+		
+		// Event Handling
+		chooseButton1.setOnAction((actionEvent) -> {
+			File selectedFile = file1.showOpenDialog(stage);
+			try {
+				filepath1 = selectedFile.toString();
+				label.setText(filepath1);
+			}
+			catch(NullPointerException e) { // Closing without selecting a file.	
+			}
+		});
+		
+		chooseButton2.setOnAction((actionEvent) -> {
+			File selectedFile = file1.showOpenDialog(stage);
+			try {
+				filepath2 = selectedFile.toString();
+				label.setText(filepath2);
+			}
+			catch(NullPointerException e) { // Closing without selecting a file.	
+			}
+		});
+		
+		compareFiles.setOnAction((actionEvent) -> {
+			boolean result  = FileUtils.compareFiles(filepath1, filepath2);
+			label.setText(String.valueOf(result));
+		
+		});
+	}	
 }
